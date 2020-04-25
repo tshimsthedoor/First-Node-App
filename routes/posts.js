d250f8245a2165c9feddddf626623d0f3f8edb91
tshimsthedoor <tshims79@gmail.com>
2020-04-26 01:13:10 +0200

    I added functionon route posts and it work about CRUD

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+/* GET posts index /posts */
 router.get('/', (req, res, next) => {
   res.send('/posts');
+});/* GET posts new /posts/new */
+router.get('/new', (req, res, next) => {
+  res.send('/posts/new');
+});/* POST posts create /posts */
+router.post('/', (req, res, next) => {
+  res.send('CREATE /posts');
+});/* GET posts show /posts/:id */
+router.get('/:id', (req, res, next) => {
+  res.send('SHOW /posts/:id');
+});/* GET posts edit /posts/:id/edit */
+router.get('/:id/edit', (req, res, next) => {
+  res.send('EDIT/posts/:id/edit');
+});/* PUT posts update /posts/:id */
+router.put('/new', (req, res, next) => {
+  res.send('UPDATE /posts/:id');
+});/* DELETE posts destroy /posts/:id */
+router.delete('/:id', (req, res, next) => {
+  res.send('DELETE /posts/:id');
 });
 
 module.exports = router;
